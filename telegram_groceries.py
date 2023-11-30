e01d6e904a1bdd981599491535af24d9b7c8ef5d
mjdesrosiers <mjd2dh@virginia.edu>
2023-11-29 22:38:10 -0500

    Adding further content

--- telegram_groceries.py
+++ telegram_groceries.py
@@ -1,4 +1,4 @@
-import printer_server
+from printer_server import send_message_to_server
 from config_loader import config
 from telethon.sync import TelegramClient
 
@@ -6,17 +6,17 @@ from telethon.sync import TelegramClient
 def get_telegram_messages():
     api_id = config['api_id']
     api_hash = config['api_hash']
-    main_chat_id = config['main_chat_id']
+    main_chat_id = config['grocery_chat_id']
 
     client = TelegramClient('session_name', api_id, api_hash)
     client.start()
 
     # gets most recent message
-    message = client.get_messages(main_chat_id)
+    message = client.get_messages(main_chat_id)[0].message
     return [message]
 
 
 if __name__ == "__main__":
     messages = get_telegram_messages()
     text = "\n".join(messages)
-    printer_server.send_message_to_server(text)
+    send_message_to_server(text)
