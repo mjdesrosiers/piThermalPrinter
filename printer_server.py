e7dbe0a50ce4fbf74029c74aa691fae5a7634be7
mjdesrosiers <mjd2dh@virginia.edu>
2023-12-03 21:01:33 -0500

    Adding further content

--- printer_server.py
+++ printer_server.py
@@ -36,7 +36,7 @@ def get_telegram_messages():
     asyncio.set_event_loop(loop)
     client = TelegramClient('session_name', api_id, api_hash, loop=loop)
     client.start()
-    item_set = {}
+    item_set = set()
     chat_id = config['grocery_chat_id']
     for message in client.iter_messages(chat_id):
         items = message.text.split('\n')
